c2afe6c3670c5f8c769263db240e6fde9221aeb4
Felipe Santiago <jfelipe.sfilho@gmail.com>
2020-06-03 23:37:44 -0300

    Allow bot to text user upon group join

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	botapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -26,11 +27,38 @@ func main() {
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		//TODO listen to the channel where challenges get completed
+		if update.Message.NewChatMembers != nil {
+			for _, user := range *update.Message.NewChatMembers {
+				//TODO extract the hardcoded messages
+				m := fmt.Sprintf("Bem vinda ao DevOps Recife %s!\nEnviei um desafio para você no chat privado e espero que você me retorne em até 60 segundos ou terei que te convidar para sair do grupo! Nada pessoal, só não aceitamos spammers! :P", user.UserName)
+				msg := botapi.NewMessage(int64(user.ID), m)
+				bot.Send(msg)
 
-		msg := botapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+				//TODO generate a random challenge
+				msg = botapi.NewMessage(int64(user.ID), fmt.Sprintf("Quanto é 4 + 4? Você tem 60 segundos.", user.UserName))
+				bot.Send(msg)
+				//TODO add user to the active challenge list
+				//TODO start a goroutine to count the time and check if user passes the test
+			}
 
-		bot.Send(msg)
+			// msg.ReplyToMessageID = update.Message.MessageID
+		}
+		if update.Message.Chat.IsPrivate() {
+			//TODO check if there are challenges currently active for the given user and retrieve them
+			fmt.Println("")
+			var msg botapi.MessageConfig
+			//TODO validate the answer according to the challenge
+			if update.Message.Text == "8" {
+				msg = botapi.NewMessage(update.Message.Chat.ID, "Resposta correta! Obrigado!")
+				//TODO remove user from active challenge list
+			} else {
+				msg = botapi.NewMessage(update.Message.Chat.ID, "Não foi dessa vez, quer tentar de novo?")
+			}
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+		}
+
+		//TODO wipe all messages if a user post in the group and has an active challenge.
 	}
 }
