b886af4a7cd650f4108d0d04fc4024f2938b8695
Felipe Santiago <jfelipe.sfilho@gmail.com>
2020-06-07 12:04:23 -0300

    Add tests for checking user challenge answer

--- main_test.go
+++ main_test.go
@@ -5,13 +5,92 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"testing"
 
+	"github.com/fsantiag/mussum/challenge"
+	"github.com/fsantiag/mussum/language"
 	"github.com/fsantiag/mussum/mocks"
 	botapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestUserPassesTheChallenge(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockBotApi := mocks.NewMockBotIface(mockCtrl)
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stdout)
+	}()
+
+	c := challenge.Generate()
+	u := botapi.Update{
+		Message: &botapi.Message{
+			Chat: &botapi.Chat{
+				ID: 1,
+			},
+			Text: strconv.Itoa(c.Result),
+			From: &botapi.User{
+				ID: 1,
+			},
+			MessageID: 10,
+		},
+	}
+	m := map[int]challenge.SumChallenge{
+		1: c,
+	}
+	l := language.Pt{}
+
+	msg := botapi.NewMessage(u.Message.Chat.ID, l.Correct())
+	msg.ReplyToMessageID = u.Message.MessageID
+
+	mockBotApi.EXPECT().Send(msg).Return(botapi.Message{}, nil).Times(1)
+	verifyUserAnswer(u, c, m, l, mockBotApi)
+	assert.Contains(t, buf.String(), "[1] User successfully solved the challange")
+	_, ok := m[u.Message.From.ID]
+	assert.False(t, ok)
+}
+
+func TestUserFailsTheChallenge(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockBotApi := mocks.NewMockBotIface(mockCtrl)
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stdout)
+	}()
+
+	c := challenge.Generate()
+	u := botapi.Update{
+		Message: &botapi.Message{
+			Chat: &botapi.Chat{
+				ID: 1,
+			},
+			Text: "wrong answer",
+			From: &botapi.User{
+				ID: 1,
+			},
+			MessageID: 10,
+		},
+	}
+	m := map[int]challenge.SumChallenge{
+		1: c,
+	}
+	l := language.Pt{}
+
+	msg := botapi.NewMessage(u.Message.Chat.ID, l.Wrong())
+	msg.ReplyToMessageID = u.Message.MessageID
+
+	mockBotApi.EXPECT().Send(msg).Return(botapi.Message{}, nil).Times(1)
+	verifyUserAnswer(u, c, m, l, mockBotApi)
+	assert.Contains(t, buf.String(), "[1] Wrong answer for challenge")
+	_, ok := m[u.Message.From.ID]
+	assert.True(t, ok)
+}
 
 func TestKickUser(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
