a777eb0412ec3025333fc9532ce83fe8d1f80e41
Felipe Santiago <jfelipe.sfilho@gmail.com>
2020-06-13 14:09:10 -0300

    Fine tunning in the portuguese mussum quotes

--- language/pt.go
+++ language/pt.go
@@ -3,13 +3,13 @@ package language
 type pt struct{}
 
 func (p pt) Welcome() string {
-	return "Bem vindis ao DevOps Recife @%v! Sou o moderadorzis do grupis e estouzis aqui paris garantirzis que não teremis spammers. Te enviis um desafiis e esperis que você me retorne em até 60 segundis ou terei que te convidarzis paris sairzis do grupis!"
+	return "Bem vindis ao DevOps Recife @%v! Sou o moderadorzis do grupis e estou aqui para garantirzis que não teremos spammers. Te enviarei um desafiis e espero que você me retornis em até 60 segundis ou terei que te convidarzis para sair do grupis!"
 }
 func (p pt) Wrong() string {
 	return "Cacildis! Resposta incorretis! Que tentar de novis?"
 }
 func (p pt) Correct() string {
-	return "Respostis corretis! Viva a liberdadis!"
+	return "Resposta corretis! Viva a liberdadis!"
 }
 func (p pt) Challenge() string {
 	return "Qualzis o valorzis de %v %v %v?"
